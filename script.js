7853e27c826b0c8e884b8c8ab0c0277302b7478a
lettyfranca <leeffrance@gmail.com>
2022-07-21 00:12:18 -0300

    versão final

--- script.js
+++ script.js
@@ -2,3 +2,36 @@
  * Jokenpô com JavaScript
  * @author Letícia França
  */
+
+function jogar() {
+    if (document.querySelector("#pedra").checked == false && document.querySelector("#papel").checked == false && document.querySelector("#tesoura").checked == false) {
+        alert("Selecione uma opção!")
+    } else {
+        //lógica principal
+        let sorteio = Math.floor(Math.random() * 3);
+        switch (sorteio) {
+            case 0:
+                document.querySelector("#pc").src = "/img/pcpedra.png";
+                break;
+            case 1:
+                document.querySelector("#pc").src = "/img/pcpapel.png";
+                break;
+            case 2:
+                document.querySelector("#pc").src = "/img/pctesoura.png";
+                break;
+        }
+        //verificar o resultado do jogo
+        if ((document.querySelector("#pedra").checked == true && sorteio == 0) || (document.querySelector("#papel").checked == true && sorteio == 1) || (document.querySelector("#tesoura").checked == true && sorteio == 2)){
+            document.querySelector("#placar").innerHTML = "Empate";
+        } else if ((document.querySelector("#pedra").checked == true && sorteio == 2) || (document.querySelector("#papel").checked == true && sorteio == 0) || (document.querySelector("#tesoura").checked == true && sorteio == 1)){ 
+            document.querySelector("#placar").innerHTML = "Jogador Venceu";
+        } else {
+            document.querySelector("#placar").innerHTML = "O computador venceu.";
+        }
+    }
+}
+
+function resetar() {
+    document.querySelector("#pc").src = "/img/pc.png";
+    document.querySelector("#placar").innerHTML = "";
+}
